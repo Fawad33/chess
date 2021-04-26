c4ea0868a9db4e7979c40f0d0d7174d5fdf78b59
Fawadul Hasan <rahathasan706@gmail.com>
2021-04-27 00:58:22 +0600

    - adds change to gameplay - adds change to king cpp

--- Gameplay.cpp
+++ Gameplay.cpp
@@ -21,25 +21,24 @@ void gameplay::display() {
 void gameplay::playGame() {
 	bool isBlackKingChecked = false, isWhiteKingChecked = false;
 	int fromX, fromY, toX, toY;
+	string turn = "white";
 	while (isBlackKingChecked == false && isWhiteKingChecked == false) {
-		cout << "White's Turn. \n Enter the co-ordinates from which you want to move: ";
+		cout << turn << "'s Turn. \n Enter the co-ordinates from which you want to move: ";
 		cin >> fromX;
 		cin >> fromY;
 		cout << "Enter the co-ordinates to which you want to move: ";
 		cin >> toX;
 		cin >> toY;
 		//function to move to be added here. should return king check to be true or false
-		if (chessBoardObj.board[fromX][fromY]->move(fromX, fromY, toX, toY) == true)
+		if (chessBoardObj.board[fromX][fromY]->getType() != turn) {
+			cout << "Select valid chess piece." << endl;
+		}
+		else if (chessBoardObj.board[fromX][fromY]->move(fromX, fromY, toX, toY) == true) {
 			chessBoardObj.setBoardValue(fromX, fromY, toX, toY);
+			if (turn == "white")
+				turn = "black";
+		}			
 		else
 			cout << "Invaild move, enter a valid move." << endl;
-		cout << "Black's Turn. \n Enter the co-ordinates from which you want to move: ";
-		cin >> fromX;
-		cin >> fromY;
-		cout << "Enter the co-ordinates to which you want to move: ";
-		cin >> toX;
-		cin >> toY;
-		//function to move to be added here
-		chessBoardObj.setBoardValue(fromX, fromY, toX, toY);
 	}
 }
--- King.cpp
+++ King.cpp
@@ -10,6 +10,6 @@ king::king(string name, string type, int id) : chessPiece(name, type, id) {
 bool king::move(int fromX, int fromY, int toX, int toY) {
 	if ((fromX || fromY || toX || toY > 7) || (fromX || fromY || toX || toY < 0))
 		return false;
-	else if(abs(fromX - toX) == 1)
-	return true;
+	if ((abs(fromX - toX) == 1 && abs(fromY - toY) == 0) || (abs(fromX - toX) == 0 && abs(fromY - toY) == 1) || (abs(fromX - toX) == 1 && abs(fromY - toY) == 1))
+		return true;
 }
